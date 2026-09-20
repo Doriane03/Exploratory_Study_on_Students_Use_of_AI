84f87209693a3628146fb056ead99f8c3df5f29f
Doriane03 <josephinedorianekouadio@gmail.com>
2026-09-20 23:08:12 +0000

    Add the script to read the file's contents

--- code/python/data_processing.py
+++ code/python/data_processing.py
@@ -4,5 +4,12 @@ file_path=os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 try:
     with open(os.path.join(file_path,'Data','Data_collected_with_google_form.csv'),mode='rt') as fic:
         print("The file is open")
+        #script to read the contents of the CSV file
+        try:
+            fic_open=csv.DictReader(fic,delimiter=',')
+            for element in fic_open :
+                print(element)
+        except  Exception as e:
+            print("The file could not be read:", csv.Error(e))
 except Exception as exc:
     print("The file could not be opened:", os.strerror(exc.errno))
